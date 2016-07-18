b160642e03191a68d1d5d39481699fd2d5d56a2d
hchsquared <hannahc.henderson@gmail.com>
2016-07-18 10:24:42 -0700

    Clean up linter issues, convert functions to es6

--- server/database/database.js
+++ server/database/database.js
@@ -1,30 +1,32 @@
-var Sequelize = require('sequelize');
-var db = new Sequelize('divvy', 'root', '', {
+const Sequelize = require('sequelize');
+const db = new Sequelize('divvy', 'root', '', {
   host: 'localhost',
   dialect: 'mysql',
 
   pool: {
     max: 5,
     min: 0,
-    idle: 10000
-  }
+    idle: 10000,
+  },
 }); // name, username, password
 
 db
   .authenticate()
-  .then(function(err) {
-    console.log('Connection has been established successfully.');
+  .then((err) => {
+    if (!err) {
+      console.log('Connection has been established successfully.');
+    }
   })
-  .catch(function (err) {
+  .catch((err) => {
     console.log('Unable to connect to the database:', err);
   });
 
 
-// TO DO: 
+// TO DO:
 //   1) Confirm Facebook user id data type
 //   2) Confirm that friend list can be sorted dynmanically via FB API
 //      (avoid storing all linked Facebook friends internally)
-var Users = db.define('Users', {
+const Users = db.define('Users', {
   fb_id: Sequelize.INTEGER,
   fname: Sequelize.STRING,
   lname: Sequelize.STRING,
@@ -32,65 +34,45 @@ var Users = db.define('Users', {
   gender: Sequelize.STRING,
   active: Sequelize.BOOLEAN,
   created: Sequelize.DATE,
-  first_login: Sequelize.DATE
+  first_login: Sequelize.DATE,
 });
 
 
 // Pool / Category of expenses, e.g. 'Trip to Rome'
-var ExpensePools = db.define('ExpensePools', {
+const ExpensePools = db.define('ExpensePools', {
   epool_id: Sequelize.INTEGER,
   name: Sequelize.STRING,
   description: Sequelize.STRING,
   created: Sequelize.DATE,
   paid: Sequelize.DATE,
-  closed: Sequelize.BOOLEAN
+  closed: Sequelize.BOOLEAN,
 });
-ExpensePools.belongsTo(Users, {foreignKey: 'entered_by_id'});
+ExpensePools.belongsTo(Users, { foreignKey: 'entered_by_id' });
 
 
 // Individual line-item item expenses
-var Expenses = db.define('Expenses', {
+const Expenses = db.define('Expenses', {
   expense_id: Sequelize.INTEGER,
-  amount: Sequelize.DECIMAL(10, 2),
+  amount: Sequelize.DECIMAL,
   created: Sequelize.DATE,
-  paid: Sequelize.DATE
+  paid: Sequelize.DATE,
 });
-Expenses.belongsTo(Users, {foreignKey: 'entered_by_id'});
-Expenses.belongsTo(Users, {foreignKey: 'paid_by_id'}); // Person that put down their card $$$
-Expenses.belongsTo(ExpensePools, {foreignKey: 'exp_pool_id'});
+Expenses.belongsTo(Users, { foreignKey: 'entered_by_id' });
+Expenses.belongsTo(Users, { foreignKey: 'paid_by_id' }); // Person that put down their card $$$
+Expenses.belongsTo(ExpensePools, { foreignKey: 'exp_pool_id' });
 
 
-// Users assigned to a single line-item expense 
+// Users assigned to a single line-item expense
 // e.g. expense 'stag dinner' has three users, all of whom are also tied
 //      to 'car rental' and one of whom is listed on 'booze'
-var Users_Expenses = db.define('User_Expensess', {
+const UsersExpenses = db.define('User_Expensess', {
   paid: Sequelize.DATE,
-  percent: Sequelize.INTEGER
+  percent: Sequelize.INTEGER,
 });
-Users.belongsToMany(Expenses, {as: 'user_id', through: 'User_Expenses'});
-Expenses.belongsToMany(Users, {as: 'expense_id', through: 'User_Expenses'});
+Users.belongsToMany(Expenses, { as: 'user_id', through: 'User_Expenses' });
+Expenses.belongsToMany(Users, { as: 'expense_id', through: 'User_Expenses' });
 
 
 // List of users within an expense pool
-var ExpensePools_Users = db.define('ExpensePool_Users', {});
-Users.belongsToMany(ExpensePools, {through: 'ExpensePools_Users'});
-
-
-// Test user sync
-Users.sync()
-  .then(function() {
-    return Users.create({fname: 'Jean', lname: 'Valjean'});
-  })
-  .then(function() {
-    return Users.findAll({ where: {fname: 'Jean', lname: 'Valjean'} });
-  })
-  .then(function(users) {
-    users.forEach(function(user) {
-      console.log(user.fname + ' exists');
-    });
-    db.close();
-  })
-  .catch(function(err) {
-    console.error(err);
-    db.close();
-  });
+const ExpensePoolsUsers = db.define('ExpensePool_Users', {});
+Users.belongsToMany(ExpensePools, { through: 'ExpensePools_Users' });
