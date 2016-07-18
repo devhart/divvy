5ce008e523f4a2a44cd8e3de3e70c3967bb6cd01
hchsquared <hannahc.henderson@gmail.com>
2016-07-18 16:14:23 -0700

    Lay out Sequelize relationships, enhance syntax
    Previous syntax incorrect

--- server/database/database.js
+++ server/database/database.js
@@ -8,24 +8,13 @@ const db = new Sequelize('divvy', 'root', null, {
     min: 0,
     idle: 10000,
   },
-}); // name, username, password
-
-// db
-//   .authenticate()
-//   .then((err) => {
-//     if (!err) {
-//       console.log('Connection has been established successfully.');
-//     }
-//   })
-//   .catch((err) => {
-//     console.log('Unable to connect to the database:', err);
-//   });
-
+});
 
 // TO DO:
 //   1) Confirm Facebook user id data type
 //   2) Confirm that friend list can be sorted dynmanically via FB API
 //      (avoid storing all linked Facebook friends internally)
+// MODEL: USERS
 const Users = db.define('Users', {
   user_id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
   fb_id: Sequelize.INTEGER,
@@ -39,7 +28,7 @@ const Users = db.define('Users', {
   first_login: Sequelize.DATE,
 });
 
-
+// MODEL: EXPENSE POOLS
 // Pool / Category of expenses, e.g. 'Trip to Rome'
 const ExpensePools = db.define('ExpensePools', {
   epool_id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
@@ -49,10 +38,9 @@ const ExpensePools = db.define('ExpensePools', {
   paid: Sequelize.DATE,
   closed: Sequelize.BOOLEAN,
 });
-Users.belongsTo(ExpensePools, { foreignKey: 'entered_by_id' });
-ExpensePools.hasOne(Users, { foreignKey: 'entered_by_id' });
+ExpensePools.hasOne(Users, { foreignKey: 'entered_by' });
 
-
+// MODEL: EXPENSES
 // Individual line-item item expenses
 const Expenses = db.define('Expenses', {
   expense_id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
@@ -60,17 +48,16 @@ const Expenses = db.define('Expenses', {
   created: Sequelize.DATE,
   paid: Sequelize.DATE,
 });
-// One person pays for each expense
-Users.belongsTo(Expenses, { foreignKey: 'paid_by_id' });
+ExpensePools.hasMany(Expenses, { foreignKey: 'exp_pool_id' });
+Expenses.belongsTo(ExpensePools, { foreignKey: 'exp_pool_id' });
 Expenses.hasOne(Users, { foreignKey: 'paid_by_id' });
-// One person enters each expense
-Users.belongsTo(Expenses, { foreignKey: 'entered_by_id' });
 Expenses.hasOne(Users, { foreignKey: 'entered_by_id' });
 
+// ExpensePools_Users
+ExpensePools.belongsToMany(Users, { as: 'ExpensePools', through: 'ExpensePools_Users', foreignKey: 'epool_id' });
+Users.belongsToMany(ExpensePools, { as: 'Users', through: 'ExpensePools_Users', foreignKey: 'user_id' });
 
-// ExpensePools.belongsToMany('Expenses', { foreignKey: 'exp_pool_id' });
-// Expenses.belongsTo('ExpensePools', { foreignKey: 'exp_pool_id' });
-
+// MODEL: USERSEXPENSES
 // Users assigned to a single line-item expense
 // e.g. expense 'stag dinner' has three users, all of whom are also tied
 //      to 'car rental' and one of whom is listed on 'booze'
@@ -78,11 +65,8 @@ const UsersExpenses = db.define('UserExpensess', {
   paid: Sequelize.DATE,
   percent: Sequelize.INTEGER,
 });
-// Users.hasMany(Expenses, { as: 'user_id', through: 'UsersExpenses' });
-// Expenses.hasMany(Users, { as: 'expense_id', through: 'UsersExpenses' });
+Users.belongsToMany(Expenses, { as: 'Users', through: UsersExpenses, foreignKey: 'user_id' });
+Expenses.belongsToMany(Users, { as: 'Expenses', through: UsersExpenses, foreignKey: 'expense_id' });
 
-// List of users within an expense pool
-const ExpensePoolsUsers = db.define('ExpensePoolUsers', {});
-Users.belongsToMany(ExpensePools, { as: 'user_id', through: 'ExpensePoolsUsers' });
 
 module.exports = db;
