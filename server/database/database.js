8a565cb81eb17b5ae61f272175e275f21ab77c65
hchsquared <hannahc.henderson@gmail.com>
2016-07-18 15:13:15 -0700

    Clean up basic models, launch Sequelize from Server

--- server/database/database.js
+++ server/database/database.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('divvy', 'root', '', {
+const db = new Sequelize('divvy', 'root', null, {
   host: 'localhost',
   dialect: 'mysql',
-
+  freezeTableName: true,
   pool: {
     max: 5,
     min: 0,
@@ -10,16 +10,16 @@ const db = new Sequelize('divvy', 'root', '', {
   },
 }); // name, username, password
 
-db
-  .authenticate()
-  .then((err) => {
-    if (!err) {
-      console.log('Connection has been established successfully.');
-    }
-  })
-  .catch((err) => {
-    console.log('Unable to connect to the database:', err);
-  });
+// db
+//   .authenticate()
+//   .then((err) => {
+//     if (!err) {
+//       console.log('Connection has been established successfully.');
+//     }
+//   })
+//   .catch((err) => {
+//     console.log('Unable to connect to the database:', err);
+//   });
 
 
 // TO DO:
@@ -27,9 +27,11 @@ db
 //   2) Confirm that friend list can be sorted dynmanically via FB API
 //      (avoid storing all linked Facebook friends internally)
 const Users = db.define('Users', {
+  user_id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
   fb_id: Sequelize.INTEGER,
   fname: Sequelize.STRING,
   lname: Sequelize.STRING,
+  email: Sequelize.STRING,
   img_file: Sequelize.STRING,
   gender: Sequelize.STRING,
   active: Sequelize.BOOLEAN,
@@ -40,39 +42,47 @@ const Users = db.define('Users', {
 
 // Pool / Category of expenses, e.g. 'Trip to Rome'
 const ExpensePools = db.define('ExpensePools', {
-  epool_id: Sequelize.INTEGER,
+  epool_id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
   name: Sequelize.STRING,
   description: Sequelize.STRING,
   created: Sequelize.DATE,
   paid: Sequelize.DATE,
   closed: Sequelize.BOOLEAN,
 });
-ExpensePools.belongsTo(Users, { foreignKey: 'entered_by_id' });
+Users.belongsTo(ExpensePools, { foreignKey: 'entered_by_id' });
+ExpensePools.hasOne(Users, { foreignKey: 'entered_by_id' });
 
 
 // Individual line-item item expenses
 const Expenses = db.define('Expenses', {
-  expense_id: Sequelize.INTEGER,
+  expense_id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
   amount: Sequelize.DECIMAL,
   created: Sequelize.DATE,
   paid: Sequelize.DATE,
 });
-Expenses.belongsTo(Users, { foreignKey: 'entered_by_id' });
-Expenses.belongsTo(Users, { foreignKey: 'paid_by_id' }); // Person that put down their card $$$
-Expenses.belongsTo(ExpensePools, { foreignKey: 'exp_pool_id' });
+// One person pays for each expense
+Users.belongsTo(Expenses, { foreignKey: 'paid_by_id' });
+Expenses.hasOne(Users, { foreignKey: 'paid_by_id' });
+// One person enters each expense
+Users.belongsTo(Expenses, { foreignKey: 'entered_by_id' });
+Expenses.hasOne(Users, { foreignKey: 'entered_by_id' });
 
+
+// ExpensePools.belongsToMany('Expenses', { foreignKey: 'exp_pool_id' });
+// Expenses.belongsTo('ExpensePools', { foreignKey: 'exp_pool_id' });
 
 // Users assigned to a single line-item expense
 // e.g. expense 'stag dinner' has three users, all of whom are also tied
 //      to 'car rental' and one of whom is listed on 'booze'
-const UsersExpenses = db.define('User_Expensess', {
+const UsersExpenses = db.define('UserExpensess', {
   paid: Sequelize.DATE,
   percent: Sequelize.INTEGER,
 });
-Users.belongsToMany(Expenses, { as: 'user_id', through: 'User_Expenses' });
-Expenses.belongsToMany(Users, { as: 'expense_id', through: 'User_Expenses' });
-
+// Users.hasMany(Expenses, { as: 'user_id', through: 'UsersExpenses' });
+// Expenses.hasMany(Users, { as: 'expense_id', through: 'UsersExpenses' });
 
 // List of users within an expense pool
-const ExpensePoolsUsers = db.define('ExpensePool_Users', {});
-Users.belongsToMany(ExpensePools, { through: 'ExpensePools_Users' });
+const ExpensePoolsUsers = db.define('ExpensePoolUsers', {});
+Users.belongsToMany(ExpensePools, { as: 'user_id', through: 'ExpensePoolsUsers' });
+
+module.exports = db;
--- server/server.js
+++ server/server.js
@@ -4,12 +4,19 @@ import 'babel-polyfill';
 import configExpress from './config/express-config';
 import configRoutes from './routes';
 import express from 'express';
+import db from './database/database';
 
 const app = express();
+const port = 3000;
 
 configExpress(app);
 configRoutes(app);
 
-app.get( '/', (req, res) => res.send('Hello World!') );
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen( 3000, () => console.log('Example app listening on port 3000!') );
+db.sync().then(() => {
+  app.listen(port, () => console.log('Example app listening on port 3000!'));
+  // const server = app.listen(app.get('port'), function() {
+  //   debug('Express server listening on port ' + server.address().port);
+  // });
+});
