613cc6a3870adee8a98a7766626fa48a09786071
hchsquared <hannahc.henderson@gmail.com>
2016-07-18 23:52:25 -0700

    Complete all SQL statements for routes

--- server/database/queries.js
+++ server/database/queries.js
@@ -1,6 +1,8 @@
 // Queries necessary for selected routes
 //    Note: values must be provided to the strSQL statements
 //          by using the variables provided
+//    Note: What is the friends situation? Pulling from FB or
+//          tracking internally?
 // See bottom of page for query vs. insert helper functions
 //    They take strSQL as an argument
 
@@ -12,7 +14,8 @@ var strSQL = '';
 var epool_id, name, description, paid, entered_by_id, closed;
 
 strSQL = "INSERT INTO expense_pools (name, description, paid, entered_by_id, closed) VALUES " +
-         "'" + name + "', '" + description + "', " +  paid + ", " + entered_by_id + ", " + closed + ";";
+         "'" + name + "', '" + description + "', " +  paid + ", " +
+         entered_by_id + ", " + closed + ";";
 
 // get => get a specific pool
 // /api/expense-pools/:id
@@ -30,13 +33,11 @@ strSQL = "UPDATE expense_pools (epool_id, name, description, paid, entered_by_id
 strSQL = "SELECT * FROM expenses WHERE epool_id = " + epool_id + ";";
 
 // POST => add an expense to a specific pool
-// /api/expense-pools/id/expenses 
+// /api/expense-pools/id/expenses
 var amount, paid_by_id;
 strSQL = "INSERT INTO expenses (name, description, amount, paid_by_id, entered_by_id, epool_id) " +
-         "VALUES ('" + name + "', '" + description + "', " + amount + ", " + paid_by_id + ", " + entered_by_id + ", " + epool_id + ");";
-
-// TODO: Find friends
-//       Facebook API? 
+         "VALUES ('" + name + "', '" + description + "', " +
+         amount + ", " + paid_by_id + ", " + entered_by_id + ", " + epool_id + ");";
 
 // Check to to see existence of user (returns empty result if no match)
 var email;
@@ -48,14 +49,19 @@ strSQL = "SELECT * FROM users;";
 // /api/users/:id
 strSQL = "SELECT * FROM users WHERE user_id = " + user_id + ";";
 
-/api/users/:id/friends
+// /api/users/:id/expenses
+// /api/users/:id/expenses-users
+strSQL = "SELECT * FROM expenses INNER JOIN users_expenses ON expenses.expense_id = users_expenses.expense_id WHERE user_id = " + user_id + ";";
 
-/api/users/:id/expenses
-/api/users/:id/expenses-users
-/api/users/:id/users-expense-pools
+// /api/users/:id/users-expense-pools
+strSQL = "elect * from expense_pools INNER JOIN expense_pools_users ON expense_pools.epool_id = expense_pools_users.epool_id WHERE user_id = " + user_id + ";";
+
+// TODO: Find friends
+//       Facebook API?
+// /api/users/:id/friends
 
 
-
+// HELPER QUERY FUNCTIONS
   // queryDb: function(strSQL, callback) {
   //   db.query(strSQL, function(err, rows, fields) {
   //     if (err) {
@@ -64,7 +70,7 @@ strSQL = "SELECT * FROM users WHERE user_id = " + user_id + ";";
   //     } else {
   //       var str = JSON.stringify(rows);
   //       rows = JSON.parse(str);
-  //       callback(err, rows); 
+  //       callback(err, rows);
   //     }
   //   });
   // },
@@ -77,6 +83,5 @@ strSQL = "SELECT * FROM users WHERE user_id = " + user_id + ";";
   //     } else {
   //       callback(err, 'success');
   //     }
-      
   //   });
-  // }  
+  // }
