9a6211b10de2fc2de3267f31b739ac8cd4ca0613
hchsquared <hannahc.henderson@gmail.com>
2016-07-19 12:37:29 -0700

    Incorporate all scaffolding updates to expense-pools and expenses

--- server/routes.js
+++ server/routes.js
@@ -1,8 +1,9 @@
 import authRouter from './auth';
 import userRouter from './api/user';
+import expensePoolRouter from './api/expense-pool';
 
 export default app => {
   app.use('/api/users', userRouter);
-
+  app.use('/api/expense-pools', expensePoolRouter);
   app.use('/auth', authRouter);
 };
