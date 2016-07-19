186f4a884b6705112be22cb734345f4099d20640
James Ramadan <jamesramadan@gmail.com>
2016-07-19 10:59:09 -0700

    Pool Page - In progress, partially complete,  & functioning. Pausing to team-up with Wayne.

--- client/src/app/components/pool/pool.js
+++ client/src/app/components/pool/pool.js
@@ -1,5 +1,5 @@
 angular.module('poolApp', [])
-.controller('poolCtrl', function($scope) {
+.controller('poolCtrl', function($scope, $state) {
 
 	$scope.expenses = [{expense: 'salad/salad dressing', amount: 10, participated: "yes"}, {expense: 'chips & salsa', amount: 20, participated: "yes"}, 
 	{expense: 'hotdog supplies', amount: 30, participated: "yes"}];
@@ -10,6 +10,10 @@ angular.module('poolApp', [])
 		} else {
 			$scope.expenses[index].participated = "yes";
 		}
+	};
+
+	$scope.toAddExpense = function () {
+		$state.go('newExpensesState');
 	};
 
 });
