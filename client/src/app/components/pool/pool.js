5f7b1145df919d7f042d12b412e7b61f45a491b1
James Ramadan <jamesramadan@gmail.com>
2016-07-18 17:09:51 -0700

    Much progress on Pool Files

--- client/src/app/components/pool/pool.js
+++ client/src/app/components/pool/pool.js
@@ -1,4 +1,15 @@
 angular.module('poolApp', [])
 .controller('poolCtrl', function($scope) {
 
+	$scope.expenses = [{expense: 'salad/salad dressing', amount: 10, participated: "yes"}, {expense: 'chips & salsa', amount: 20, participated: "yes"}, 
+	{expense: 'hotdog supplies', amount: 30, participated: "yes"}];
+
+	$scope.participateClick = function (participated, index) {
+		if(participated === "yes") {
+			$scope.expenses[index].participated = "no";
+		} else {
+			$scope.expenses[index].participated = "yes";
+		}
+	};
+
 });
