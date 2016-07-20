1e720df5b6dd04417993c3d1e5153946a2213345
James Ramadan <jamesramadan@gmail.com>
2016-07-20 14:10:55 -0700

    Client Folder Restructering. Bower Installed. (#102)

---
+++ client/app/components/newExpenses/newExpenses.js
@@ -0,0 +1,24 @@
+angular.module('newExpenseApp', [])
+.controller('newExpensesCtrl', function($scope, $state, db, $stateParams) {
+
+  $scope.currentPool = '';
+  
+  $scope.toPool = function () {
+    $state.go('poolState');
+  };
+
+  $scope.addExpense = function (expense) {
+    console.log('adding a new expense', expense);
+    expense.id = Math.floor(Math.random()*10000);
+    expense.createdBy = 1;
+    expense.participated = "yes";
+
+    for (var i =0; i < db.pools.length; i++) {
+      if (db.pools[i].id === +$stateParams.id) {
+        db.pools[i].expenses.push(expense);
+      }
+    } 
+    $state.go('poolState', {id: +$stateParams.id});
+  };
+
+});
