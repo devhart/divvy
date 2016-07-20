5e0330bac1dd442fc07545602253366299fe26cf
DTing <dennisting@gmail.com>
2016-07-20 10:39:11 -0700

    Add some utility modules to server and rename express config file (#95)
    * Add server utility modules
    
    compression: https://www.npmjs.com/package/compression
    errorhandler: https://www.npmjs.com/package/errorhandler
    
    * Rename express-config to express
    
    express-config.js is already inside the config
    folder, removing redundancy.
    
    routes.md's contents has been incorporated into
    the index files for each api.

--- server/config/express.js
+++ server/config/express.js
@@ -1,5 +1,7 @@
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import errorHandler from 'errorhandler';
 import express from 'express';
 import morgan from 'morgan';
 
@@ -12,7 +14,12 @@ export default app => {
 
   app.use(morgan('dev'));
   app.use(express.static(app.get('clientPath')));
+  app.use(compression());
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(cookieParser());
+
+  if (app.get('env') === 'development' || app.get('env') === 'test') {
+    app.use(errorHandler());
+  }
 };
