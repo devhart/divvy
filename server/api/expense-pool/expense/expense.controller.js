9fd40f205c9e3873cd62bba6b896d63c7c72d6f3
Hannah Henderson <hannahc.henderson@gmail.com>
2016-07-20 10:50:06 -0700

    Query Sequelize through Expense-Pool and Expense controllers (#90)
    * Query Sequelize through Expense-Pool and Expense controllers
    
    * Change route names to indicate those used for testing only

--- server/api/expense-pool/expense/expense.controller.js
+++ server/api/expense-pool/expense/expense.controller.js
@@ -1,22 +1,75 @@
+import { Expense } from '../../../database/database';
+
 const controller = {};
 
 controller.addExpense = (req, res) => {
-  res.send('addExpense');
+  Expense.create({
+    name: 'dummy expense',
+    description: 'test posting',
+    amount: 40.52,
+    paid: false,
+  }, ['name', 'description', 'amount', 'paid'])
+  .then(expense => {
+    res.send(expense.get({
+      plain: true,
+    }));
+  })
+  .catch(error => {
+    console.log(error);
+    res.sendStatus(500);
+  });
 };
 
 // GET => All expenses
+// TODO: Fix to return info
 controller.getExpenses = (req, res) => {
-  res.send('getExpenses - all');
+  Expense.findAll({
+    raw: true,
+  })
+  .then(expenses => {
+    res.send(expenses);
+  })
+  .catch(error => {
+    console.log(error);
+    res.sendStatus(500);
+  });
 };
 
 // POST => update a specific expense
 controller.editExpense = (req, res) => {
+  Expense.update(
+    {
+      name: 'new updated EXPENSE name',
+      description: 'new description',
+    },
+    {
+      where: { _id: req.body.id },
+    })
+  .then(expense => {
+    res.send(expense.get({
+      plain: true,
+    }));
+  })
+  .catch(error => {
+    console.log(error);
+    res.sendStatus(500);
+  });
   res.send('editExpense');
 };
 
 // GET => specific expense
 controller.getOneExpense = (req, res) => {
-  res.send(`getOneExpense with expenseId: ${req.params.expenseId}`);
+  Expense.findAll({
+    where: { _id: req.params.expenseId },
+    raw: true,
+  })
+  .then(expenses => {
+    res.send(expenses);
+  })
+  .catch(error => {
+    console.log(error);
+    res.sendStatus(500);
+  });
 };
 
 export default controller;
