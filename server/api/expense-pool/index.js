50153d5fc0685258193d5158c4c3a4461aa66145
Dennis Ting <dennisting@gmail.com>
2016-07-21 09:29:53 -0700

    Add ExpensePool member authorization middleware.
    Resolve #113

--- server/api/expense-pool/index.js
+++ server/api/expense-pool/index.js
@@ -2,32 +2,34 @@ import express from 'express';
 import controller from './expense-pool.controller';
 import expenseRouter from './expense';
 
+import { isPoolMember } from './expense-pool.auth';
+
 const router = express.Router();
 
 // POST => add a new pool
 router.post('/', controller.addPool);
 
 // GET => get a specific pool
-router.get('/:id', controller.getPool);
+router.get('/:id', isPoolMember(), controller.getPool);
 
 /**
  * POST => Add user to pool
  * Expect body to contain ID of user to add
  */
-router.post('/:id/users', controller.addUserToPool);
+router.post('/:id/users', isPoolMember(), controller.addUserToPool);
 
 // ------------ Routes above have updated controllers ----------------
 
 // GET => show all pools
 // Added for testing only
-router.get('/test', controller.getPools);
+router.get('/test', isPoolMember(), controller.getPools);
 
 // Express router captures implied filepath
 // '/:id' below is '/api/expense-pools/:id'
 
 // POST => update a specific pool
-router.post('/:id', controller.updatePool);
+router.post('/:id', isPoolMember(), controller.updatePool);
 
-router.use('/:id/expenses', expenseRouter);
+router.use('/:id/expenses', isPoolMember(), expenseRouter);
 
 export default router;
