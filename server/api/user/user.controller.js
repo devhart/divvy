9358dd3dc2e251cb4b957163b1cccf62b3e4833a
DTing <dennisting@gmail.com>
2016-07-21 18:57:19 -0700

    Update user routes and controller (#123)
    - Remove unused routes from user.controller.
    - Change route to get all expense pools for the
      request user: GET `/api/users/me/expense-pools/`.

--- server/api/user/user.controller.js
+++ server/api/user/user.controller.js
@@ -1,46 +1,33 @@
 import { User } from '../../db';
-import { handleError } from '../../utils';
+import { handleError, handleNotFound } from '../../utils';
 
 const controller = {};
 
-controller.me = (req, res) => {
-  res.json(req.user);
-};
-
-controller.getUserPools = (req, res) => {
-  req.user.getExpensePools()
-    .then(expensePools => res.json(expensePools))
-    .catch(handleError(res));
-};
-
-// Returns current user, and users in pool
-controller.getAllUsers = (req, res) => {
+controller.all = (req, res) => {
   User.findAll()
     .then(users => res.json(users))
     .catch(handleError(res));
 };
 
-
-// ------------ Routes above have updated controllers ----------------
-
-controller.getUserPoolExpenses = (req, res) => {
-  res.send(`getUserPoolExpenses ${req.params.id}, ${req.params.expPoolId}`);
+controller.get = (req, res) => {
+  User.findById(req.params.id)
+    .then(handleNotFound(res))
+    .then(user => {
+      if (user) {
+        res.json(user);
+      }
+    })
+    .catch(handleError(res));
 };
 
-controller.getUserExpenses = (req, res) => {
-  res.send(`getUserExpenses for user id: ${req.params.id}`);
+controller.me = (req, res) => {
+  res.json(req.user);
 };
 
-controller.getUser = (req, res) => {
-  res.send(`getUser for user id: ${req.params.id}`);
-};
-
-controller.addUserExpense = (req, res) => {
-  res.send('addUserExpense');
-};
-
-controller.addUser = (req, res) => {
-  res.send('addUser');
+controller.myPools = (req, res) => {
+  req.user.getExpensePools()
+    .then(expensePools => res.json(expensePools))
+    .catch(handleError(res));
 };
 
 export default controller;
