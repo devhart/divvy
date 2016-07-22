acc89cb0acbeb19e8a6b9ba8bafc4e16f27227f6
Dennis Ting <dennisting@gmail.com>
2016-07-21 22:33:15 -0700

    Fix order of user api route definition
    Resolves # 133

--- server/api/user/index.js
+++ server/api/user/index.js
@@ -1,8 +1,8 @@
 /**
  * GET   /api/users                    -> list of all users
- * GET   /api/users/:id                -> get info for req.params.id user
  * GET   /api/users/me                 -> get info for req.user
  * GET   /api/users/me/expense-pools/  -> list of expense pools for req.user
+ * GET   /api/users/:id                -> get info for req.params.id user
  */
 
 import express from 'express';
@@ -10,10 +10,11 @@ import controller from './user.controller';
 const router = express.Router();
 
 router.get('/', controller.all);
-router.get('/:id', controller.get);
 
 router.get('/me', controller.me);
 router.get('/me/expense-pools', controller.myPools);
+
+router.get('/:id', controller.get);
 
 export default router;
 
