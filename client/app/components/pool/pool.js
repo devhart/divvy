ce96cbf61e78fe71480e75c1c31fd2aa0b852f2e
Dennis Ting <dennisting@gmail.com>
2016-07-23 11:24:47 -0700

    Fix pool.js lint errors

--- client/app/components/pool/pool.js
+++ client/app/components/pool/pool.js
@@ -1,38 +1,36 @@
-angular.module('poolApp', [])
-.controller('poolCtrl', function($scope, $state, db, $stateParams) {
+var app = angular.module('poolApp', []);
 
-	$scope.expenses = [];
-	$scope.pool = {};
+app.controller('poolCtrl', function poolCtrl($scope, $state, db, $stateParams) {
+  $scope.expenses = [];
+  $scope.pool = {};
 
-	$scope.participateClick = function (participated, index) {
-		if(participated === "yes") {
-			$scope.expenses[index].participated = "no";
-		} else {
-			$scope.expenses[index].participated = "yes";
-		}
-	};
+  $scope.participateClick = function participateClick(participated, index) {
+    if (participated === 'yes') {
+      $scope.expenses[index].participated = 'no';
+    } else {
+      $scope.expenses[index].participated = 'yes';
+    }
+  };
 
-	$scope.toAddExpense = function () {
-		$state.go('newExpensesState', {id: $stateParams.id});
-	};
+  $scope.toAddExpense = function toAddExpense() {
+    $state.go('newExpensesState', { id: $stateParams.id });
+  };
 
-	$scope.toEditExpense = function (id) {
-		$state.go('updateExpensesState', {poolid: $stateParams.id, expenseid: id });
-	};
+  $scope.toEditExpense = function toEditExpense(id) {
+    $state.go('updateExpensesState', { poolid: $stateParams.id, expenseid: id });
+  };
 
-	$scope.filterToPool = function () {
-		for (var i =0; i < db.pools.length; i++) {
-			if (db.pools[i].id === +$stateParams.id) {
-				$scope.pool = db.pools[i];
-				$scope.pool.total = 0;
-				$scope.users = db.pools[i].users;
-				$scope.expenses = db.pools[i].expenses;
-				for (var j = 0; j < db.pools[i].expenses.length; j++) {
-				$scope.pool.total += +db.pools[i].expenses[j].amount;
-					
-				}
-			}
-		}		
-	};
-
+  $scope.filterToPool = function filterToPool() {
+    for (var i = 0; i < db.pools.length; i++) {
+      if (db.pools[i].id === +$stateParams.id) {
+        $scope.pool = db.pools[i];
+        $scope.pool.total = 0;
+        $scope.users = db.pools[i].users;
+        $scope.expenses = db.pools[i].expenses;
+        for (var j = 0; j < db.pools[i].expenses.length; j++) {
+          $scope.pool.total += +db.pools[i].expenses[j].amount;
+        }
+      }
+    }
+  };
 });
