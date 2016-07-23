e0cb12fe77fd288099f6d7d6c641b5cf5b6e1026
Dennis Ting <dennisting@gmail.com>
2016-07-23 11:31:05 -0700

    Fix updateExpenses.js lint errors

--- client/app/components/updateExpenses/updateExpenses.js
+++ client/app/components/updateExpenses/updateExpenses.js
@@ -1,53 +1,48 @@
-angular.module('updateExpenseApp', [])
-.controller('updateExpensesCtrl', function($scope, $state, db, $stateParams) {
+var app = angular.module('updateExpenseApp', []);
 
+app.controller('updateExpensesCtrl', function updateExpensesCtrl($scope, $state, db, $stateParams) {
   $scope.expense = {};
-    
-  $scope.toPool = function () {
-    $state.go('poolState', {id:$stateParams.poolid});
+
+  $scope.toPool = function toPool() {
+    $state.go('poolState', { id: $stateParams.poolid });
   };
 
-  $scope.deleteExpense = function () {
-    
-
-    for (var i =0; i < db.pools.length; i++) {
+  $scope.deleteExpense = function deleteExpense() {
+    for (var i = 0; i < db.pools.length; i++) {
       if (db.pools[i].id === +$stateParams.poolid) {
-        for (var j =0; j < db.pools[i].expenses.length; j++) {
-          if(db.pools[i].expenses[j].id === +$stateParams.expenseid)
-          db.pools[i].expenses.splice(j,1);
+        for (var j = 0; j < db.pools[i].expenses.length; j++) {
+          if (db.pools[i].expenses[j].id === +$stateParams.expenseid) {
+            db.pools[i].expenses.splice(j, 1);
+          }
         }
       }
-    } 
-
-    $state.go('poolState', {id:$stateParams.poolid});
-
+    }
+    $state.go('poolState', { id: $stateParams.poolid });
   };
 
-  $scope.editExpense = function () {
-    
-    for (var i =0; i < db.pools.length; i++) {
+  $scope.editExpense = function editExpense() {
+    for (var i = 0; i < db.pools.length; i++) {
       if (db.pools[i].id === +$stateParams.poolid) {
-        for (var j =0; j < db.pools[i].expenses.length; j++) {
-          if(db.pools[i].expenses[j].id === +$stateParams.expenseid) {
+        for (var j = 0; j < db.pools[i].expenses.length; j++) {
+          if (db.pools[i].expenses[j].id === +$stateParams.expenseid) {
             db.pools[i].expenses[j].name = $scope.expense.name;
             db.pools[i].expenses[j].amount = $scope.expense.amount;
-          } 
+          }
         }
       }
     }
-
-    $state.go('poolState', {id:$stateParams.poolid});
+    $state.go('poolState', { id: $stateParams.poolid });
   };
 
-  $scope.fetchExpense = function () {
-    for (var i =0; i < db.pools.length; i++) {
+  $scope.fetchExpense = function fetchExpense() {
+    for (var i = 0; i < db.pools.length; i++) {
       if (db.pools[i].id === +$stateParams.poolid) {
-        for (var j =0; j < db.pools[i].expenses.length; j++) {
-          if(db.pools[i].expenses[j].id === +$stateParams.expenseid) {
+        for (var j = 0; j < db.pools[i].expenses.length; j++) {
+          if (db.pools[i].expenses[j].id === +$stateParams.expenseid) {
             $scope.expense = db.pools[i].expenses[j];
-          } 
+          }
         }
       }
     }
-  }
+  };
 });
