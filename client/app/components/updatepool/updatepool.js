f9fb086d57354e1061c8ce81b5f20158e3f3f623
Dennis Ting <dennisting@gmail.com>
2016-07-23 11:32:26 -0700

    Fix updatePool.js lint errors

--- client/app/components/updatepool/updatepool.js
+++ client/app/components/updatepool/updatepool.js
@@ -1,50 +1,43 @@
-angular.module('updatePoolApp', [])
-.controller('updatePoolCtrl', function($scope, $state, db, $stateParams) {
-  
+var app = angular.module('updatePoolApp', []);
+
+app.controller('updatePoolCtrl', function updatePoolCtrl($scope, $state, db, $stateParams) {
   $scope.pool = {};
 
-  $scope.toPools = function () {
+  $scope.toPools = function toPools() {
     $state.go('poolsState');
   };
 
-  $scope.closePool = function () {
-    for (var i =0; i < db.pools.length; i++) {
+  $scope.closePool = function closePool() {
+    for (var i = 0; i < db.pools.length; i++) {
       if (db.pools[i].id === +$stateParams.id) {
         db.pools[i].status = 'closed';
       }
-    } 
+    }
   };
 
-  $scope.deletePool = function () {
-    
-
-    for (var i =0; i < db.pools.length; i++) {
+  $scope.deletePool = function deletePool() {
+    for (var i = 0; i < db.pools.length; i++) {
       if (db.pools[i].id === +$stateParams.id) {
-        db.pools.splice(i,1);
+        db.pools.splice(i, 1);
       }
-    } 
-
+    }
     $state.go('poolsState');
-
   };
 
-  $scope.editPool = function () {
-    
-    for (var i =0; i < db.pools.length; i++) {
+  $scope.editPool = function editPool() {
+    for (var i = 0; i < db.pools.length; i++) {
       if (db.pools[i].id === +$stateParams.id) {
         db.pools[i] = $scope.pool;
       }
-    } 
-
+    }
     $state.go('poolsState');
   };
 
-  $scope.filterToPool = function () {
-    for (var i =0; i < db.pools.length; i++) {
+  $scope.filterToPool = function filterToPool() {
+    for (var i = 0; i < db.pools.length; i++) {
       if (db.pools[i].id === +$stateParams.id) {
         $scope.pool = db.pools[i];
       }
-    }   
+    }
   };
-
 });
