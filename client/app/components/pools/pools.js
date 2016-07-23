0c9d500798dee66a6f53ad2dfb1be73c630825b3
Dennis Ting <dennisting@gmail.com>
2016-07-23 11:28:52 -0700

    Fix pools.js lint errors

--- client/app/components/pools/pools.js
+++ client/app/components/pools/pools.js
@@ -1,25 +1,25 @@
-angular.module('poolsApp', [])
-.controller('poolsCtrl', function($scope, $state, db) {
+var app = angular.module('poolsApp', []);
 
+app.controller('poolsCtrl', function poolsCtrl($scope, $state, $stateParams, db) {
   $scope.pools = db.pools;
 
-  $scope.addPool = function () {
+  $scope.addPool = function addPool() {
     $state.go('newPoolState');
   };
 
-  $scope.goToPool = function(id) {
-    $state.go('poolState', {id: id})
+  $scope.goToPool = function goToPool(id) {
+    $state.go('poolState', { id: id });
   };
 
-  $scope.editPool = function(id) {
-    $state.go('updatePoolState', {id: id})
-  }
+  $scope.editPool = function editPool(id) {
+    $state.go('updatePoolState', { id: id });
+  };
 
-  $scope.filterToPool = function () {
-    for (var i =0; i < db.pools.length; i++) {
+  $scope.filterToPool = function filterToPool() {
+    for (var i = 0; i < db.pools.length; i++) {
       if (db.pools[i].id === +$stateParams.id) {
         $scope.expenses = db.pools[i].expenses;
       }
-    }   
+    }
   };
 });
