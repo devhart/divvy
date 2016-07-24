5f22cc078b935a29edb7f929fbf06882cc741a9e
Dennis Ting <dennisting@gmail.com>
2016-07-23 19:41:28 -0700

    Fix bug with login not redirecting
    Login was not redirecting to facebook correctly.
    This was due to a race condition by using a syncronous
    check on the currentUser in the auth module.
    
    Added async versions of getCurrentUser and isLoggedIn.
    Added logout route on client to remove token from cookie.

--- client/app/components/auth/auth.service.js
+++ client/app/components/auth/auth.service.js
@@ -1,26 +1,49 @@
 var module = angular.module('app.auth');
 
-module.factory('Auth', function Auth(User, $cookies) {
+module.factory('Auth', function Auth($cookies, $q, $window, User) {
   var currentUser = {};
   if ($cookies.get('token')) {
     currentUser = User.me();
   }
-  return {
-    logout: function logout() {
-      $cookies.remove('token');
-      currentUser = {};
-    },
-    getCurrentUser: function getCurrentUser() {
-      return currentUser;
-    },
-    isLoggedIn: function isLoggedIn() {
-      return currentUser.hasOwnProperty('name');
-    },
-    isLoggedInAsync: function isLoggedInAsync() {
-      if ($cookies.get('token')) {
-        return User.me();
-      }
-      return currentUser;
+
+  var logout = function logout() {
+    $cookies.remove('token');
+    currentUser = {};
+  };
+
+  var login = {
+    facebook: function facebook() {
+      $window.location = '/auth/facebook';
     }
+  };
+
+  var getCurrentUser = function getCurrentUser() {
+    return currentUser;
+  };
+
+  var getCurrentUserAsync = function getCurrentUserAsync() {
+    var promiseOrUser = currentUser.hasOwnProperty('$promise') ? currentUser.$promise : currentUser;
+    return $q.when(promiseOrUser).then(function handleUser(user) {
+      return user;
+    });
+  };
+
+  var isLoggedIn = function isLoggedIn() {
+    return currentUser.hasOwnProperty('name');
+  };
+
+  var isLoggedInAsync = function isLoggedInAsync() {
+    return getCurrentUserAsync().then(function checkUserLoginStatus(user) {
+      return user.hasOwnProperty('name');
+    });
+  };
+
+  return {
+    logout: logout,
+    login: login,
+    getCurrentUser: getCurrentUser,
+    getCurrentUserAsync: getCurrentUserAsync,
+    isLoggedIn: isLoggedIn,
+    isLoggedInAsync: isLoggedInAsync
   };
 });
