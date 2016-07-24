985f86191dc3ccde38c236589d300787e81120e8
DTing <dennisting@gmail.com>
2016-07-23 20:25:00 -0700

    Refactor User service out of app.js (#148)

---
+++ client/app/components/auth/user.service.js
@@ -0,0 +1,21 @@
+var app = angular.module('app.auth');
+
+app.factory('User', function User($resource) {
+  return $resource('/api/users/:userId/:controller/:selector', {
+    userId: '@_id'
+  }, {
+    me: {
+      method: 'GET',
+      params: {
+        controller: 'me'
+      }
+    },
+    expensePools: {
+      method: 'GET',
+      params: {
+        controller: 'me',
+        selector: 'expense-pools'
+      }
+    }
+  });
+});
